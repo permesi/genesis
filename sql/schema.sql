68c227536c54b19504a138a00a23823d1aff1a36
nbari <nbari@tequila.io>
2024-08-12 10:49:19 +0200

    uuid unique for client_id

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS clients CASCADE;
 CREATE TABLE clients (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    uuid UUID DEFAULT uuid_generate_v4()
+    uuid UUID DEFAULT uuid_generate_v4() UNIQUE
 );
 
 INSERT INTO clients (id, name, uuid) VALUES (0, 'unknown', '00000000-0000-0000-0000-000000000000');
